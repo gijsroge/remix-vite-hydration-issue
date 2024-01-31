b8ee87d2fdd510a5f9df226afed3b7d93f667708
Gijs Roge <rogegijs@gmail.com>
2024-01-31 10:53:23 +0100

    initial

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import "./style.css";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
+    },
+  ];
+};
 
 export default function App() {
   return (
@@ -21,6 +32,23 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links />
+      </head>
+      <body>
+        <h1>error</h1>
+        <ScrollRestoration />
+        <Scripts />
       </body>
     </html>
   );
